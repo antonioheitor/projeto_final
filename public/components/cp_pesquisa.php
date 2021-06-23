1ad960ea625099e3d5d63209bf1c6c1ced934600
Juliana Gouveia <julyanagouveya@gmail.com>
2021-06-23 12:56:27 +0100

    query pesquisa | falta query do resto

--- public/components/cp_pesquisa.php
+++ public/components/cp_pesquisa.php
@@ -8,29 +8,26 @@ $query = "SELECT temas.id_temas, temas.nome_tema, areas.id_areas FROM temas
 INNER JOIN areas
 WHERE temas.areas_id_areas = areas.id_areas;";
 
-if (isset($_GET["search_users"])) {
-$query = $query . " WHERE users.username LIKE ?";
+if (isset($_GET["procura"])) {
+$query = $query . " WHERE temas.nome_tema LIKE ?";
 }
 
 
 if (mysqli_stmt_prepare($stmt, $query)) {
-if (isset($_GET['search_users'])) {
-$procura = "%" . $_GET['search_users'] . "%";
+if (isset($_GET['procura'])) {
+$procura = "%" . $_GET['procura'] . "%";
 mysqli_stmt_bind_param($stmt, 's', $procura);
 }
 mysqli_stmt_execute($stmt);
 
-mysqli_stmt_bind_result($stmt, $id, $email, $user, $data, $role);
+mysqli_stmt_bind_result($stmt, $id, $nome, $area);
 }
 
 ?>
 
 <main class="container-fluid mt-lg-5">
-    <section class="row justify-content-center pt-3 mb-3">
-        <form class="col-11 mt-lg-4">
-            <input type="text" id="procura" class="shadow-sm" name="procura" placeholder="Desporto">
-        </form>
-    </section>
+
+
     <div class="galeria row mx-auto mb-5">
         <div class="col-6 col-md-4 col-lg-3 mb-2">
             <a href="cp_perfiltribo.php">
