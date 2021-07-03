69c544408417160e5d5b9cd2f4678095cd6dd7c3
antonioheitor <antonioh23@ua.pt>
2021-07-03 15:40:41 +0100

    posts/parte do admin done

--- public/components/cp_conversas.php
+++ public/components/cp_conversas.php
@@ -11,7 +11,20 @@
 <?php
 require_once "connections/connection.php";
 
+if (isset($_SESSION["nome"])) {
+    $USER_NAME = $_SESSION["nome"];
 
+}
+
+if (isset($_SESSION["id"])) {
+    $USER_ID = $_SESSION["id"];
+
+}
+
+if (isset($_SESSION["role"])) {
+    $USER_ROLE = $_SESSION["role"];
+
+}
 
 
 $link = new_db_connection();
@@ -20,7 +33,7 @@ $stmt = mysqli_stmt_init($link);
 
 
 $stmt = mysqli_stmt_init($link);
-$query = "SELECT grupo_id_grupo ,nome_grupo FROM users_has_grupo INNER JOIN grupo ON grupo_id_grupo = id_grupo  WHERE users_id_users = ? ;";
+$query = "SELECT grupo_id_grupo , nome_grupo FROM users_has_grupo INNER JOIN grupo ON grupo_id_grupo = id_grupo  WHERE users_id_users = ? ;";
 
 
 if (mysqli_stmt_prepare($stmt, $query)) {
@@ -36,7 +49,7 @@ if (mysqli_stmt_prepare($stmt, $query)) {
         <article class="col-11 borda_post shadow my-3 p-4">
             <div class="row">
                 <div class="col-10">
-                    <a href="chat.php?id=<?= $grupo_id_grupo ?>" class="text-decoration-none"><h1 class="mb-0 py-3">Tribo de <?= $nome_grupo ?></h1></a>
+                    <a href="chat.php?id=<?= $grupo_id_grupo ?>" class="text-decoration-none"><h2 class="mb-0 py-3">Tribo de <?= $nome_grupo ?></h2></a>
                 </div>
                 <div class="col-2 my-auto">
                     <i class="far fa-comment-dots fa-3x float-right"></i>
