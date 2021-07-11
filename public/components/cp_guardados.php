3df6f8faa33f2e1c2a1826db1abfa8380d6f3105
Joana Lopes Tavares <joanalt@ua.pt>
2021-07-11 22:36:58 +0100

    dropdown dos guardados

--- public/components/cp_guardados.php
+++ public/components/cp_guardados.php
@@ -84,11 +84,8 @@
                             <button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown"
                                     aria-haspopup="true" aria-expanded="false"></button>
                             <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                                <a class="dropdown-item" href="#" data-target="#myModal3"
-                                   data-toggle="modal">Guardar</a>
-                                <a class="dropdown-item" href="#" data-target="#myModal4" data-toggle="modal">Apagar</a>
-                                <a class="dropdown-item" href="#" data-target="#myModal5"
-                                   data-toggle="modal">Denunciar</a>
+                                <a class="dropdown-item" href="#" data-target="#myModal3" data-toggle="modal">Remover</a>
+                                <a class="dropdown-item" href="#" data-target="#myModal5" data-toggle="modal">Denunciar</a>
                             </div>
                         </div>
                     </div>
