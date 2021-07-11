c3092924163aef485bad7a6fa7aab648122f2282
Joana Lopes Tavares <joanalt@ua.pt>
2021-07-11 16:44:41 +0100

    não foi nada de mais ahaha

--- public/components/cp_homepage.php
+++ public/components/cp_homepage.php
@@ -1,4 +1,3 @@
-
 <main class="container-fluid mt-lg-5 pt-2">
     <section class="row justify-content-center mt-lg-4 pt-3" data-target="#myModal" data-toggle="modal">
         <div class="col-11 shadow-sm borda_post rounded-pill py-3">
@@ -20,23 +19,23 @@
                 </div>
 
                 <!-- CABEÇALHO DO MODAL ######################### -->
-                <form method="post" role="form" id="post-form" action="scripts/sc_new_post.php" enctype="multipart/form-data">
+                <form method="post" role="form" id="post-form" action="scripts/sc_new_post.php"
+                      enctype="multipart/form-data">
                     <div class="text-center">
-                    <label class="ml-3 mt-3 text-center">Titulo da publicação</label>
+                        <label class="ml-3 mt-3 text-center">Titulo da publicação</label>
                     </div>
                     <input class="w-50 mx-auto" name="titulopost" type="text">
                     <div class="modal-body text-center">
                         <label class="text-center">Escreve o post :)</label>
                         <input class="w-50 mx-auto" name="descpost" type="text">
                         <p class="text-center mt-4">Selecione imagem</p>
-                        <input type="file" class="form-control w-50 mx-auto bg-light border-0" name="fileToUpload" id="customFile"/>
+                        <input type="file" class="form-control w-50 mx-auto bg-light border-0" name="fileToUpload"
+                               id="customFile"/>
                         <div class="dropdown text-center mt-4">
                             <label>Escolhe a tua tribo</label>
                             <select class="form-control" name="grupo_id_grupo">
                                 <?php
                                 require_once "connections/connection.php";
-
-
 
 
                                 $link = new_db_connection();
@@ -45,9 +44,9 @@
 
 
                                 $query = "SELECT grupo_id_grupo, id_grupo, nome_grupo FROM users_has_grupo 
-INNER JOIN grupo
-ON grupo_id_grupo = id_grupo 
-WHERE users_id_users = ?;";
+                                        INNER JOIN grupo
+                                        ON grupo_id_grupo = id_grupo 
+                                        WHERE users_id_users = ?";
 
 
                                 if (mysqli_stmt_prepare($stmt, $query)) {
@@ -116,67 +115,68 @@ WHERE users_id_users = ?;";*/
         if (mysqli_stmt_prepare($stmt, $query)) {
 
 
-        mysqli_stmt_execute($stmt);
+            mysqli_stmt_execute($stmt);
 
-        mysqli_stmt_bind_result($stmt, $id_posts, $titulo_post, $conteudo_post, $imagem_post, $data_criacao_post,
-            $nome_grupo, $grupo_id_grupo, $nome_user,  $id_user, $imagem_user);
+            mysqli_stmt_bind_result($stmt, $id_posts, $titulo_post, $conteudo_post, $imagem_post, $data_criacao_post,
+                $nome_grupo, $grupo_id_grupo, $nome_user, $id_user, $imagem_user);
 
-        while (mysqli_stmt_fetch($stmt)) { ?>
-        <article class="col-11 borda_post shadow mb-4">
-            <div class="row mt-1">
+            while (mysqli_stmt_fetch($stmt)) { ?>
+                <article class="col-11 borda_post shadow mb-4">
+                    <div class="row mt-1">
 
-                <div class="col-2 col-md-2 col-lg-1 my-auto">
-                    <img src="../uploads/<?= $imagem_user ?>" class="img-fluid rounded-circle p-sm-1 border border-success">
-                </div>
-                <div class="col-8 col-sm-8 position-relative">
-                    <h4 class="pt-3"><?= $nome_user ?></h4>
-                    <p>Tribo de <?= $nome_grupo ?> * <?= $data_criacao_post ?></p>
-                </div>
-                <div class="col-2 col-lg-3 text-right my-auto">
-                    <div class="dropdown show">
-                        <div class="btn-group dropleft">
-                            <button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></button>
-                            <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                                <a class="dropdown-item" href="#" data-target="#myModal3" data-toggle="modal">Guardar</a>
-                                <a class="dropdown-item" href="#" data-target="#myModal4" data-toggle="modal">Apagar</a>
-                                <a class="dropdown-item" href="#" data-target="#myModal5" data-toggle="modal">Denunciar</a>
+                        <div class="col-2 col-md-2 col-lg-1 my-auto">
+                            <img src="../uploads/<?= $imagem_user ?>"
+                                 class="img-fluid rounded-circle p-sm-1 border border-success">
+                        </div>
+                        <div class="col-8 col-sm-8 position-relative">
+                            <h4 class="pt-3"><?= $nome_user ?></h4>
+                            <p>Tribo de <?= $nome_grupo ?> * <?= $data_criacao_post ?></p>
+                        </div>
+                        <div class="col-2 col-lg-3 text-right my-auto">
+                            <div class="dropdown show">
+                                <div class="btn-group dropleft">
+                                    <button type="button" class="btn btn-secondary dropdown-toggle"
+                                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></button>
+                                    <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                                        <a class="dropdown-item" href="#" data-target="#myModal3" data-toggle="modal">Guardar</a>
+                                        <a class="dropdown-item" href="#" data-target="#myModal4" data-toggle="modal">Apagar</a>
+                                        <a class="dropdown-item" href="#" data-target="#myModal5" data-toggle="modal">Denunciar</a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
+
+                        <div class="pt-2">
+                            <p class="font-weight-bold pl-5"><?= $titulo_post ?></p>
+
+
+                            <?php if ($imagem_post != null) {
+                                echo "<img class='text-center' src='$imagem_post'>";
+                            }
+                            ?>
+
+                            <p class="pl-5"><?= $conteudo_post ?></p>
+                            <div class="float-right pb-2">
+                                <i class="fas fa-plus-circle fa-2x" data-target="#myModal2" data-toggle="modal"></i>
+                            </div>
+                        </div>
+
                     </div>
-                </div>
-
-            <div class="pt-2">
-                <p class="font-weight-bold pl-5"><?= $titulo_post ?></p>
-
-
-                <?php if ($imagem_post != null) {
-                    echo "<img class='text-center' src='$imagem_post'>";
-                }
-                    ?>
-
-                <p class="pl-5"><?= $conteudo_post ?></p>
-                <div class="float-right pb-2">
-                    <i class="fas fa-plus-circle fa-2x" data-target="#myModal2" data-toggle="modal"></i>
-                </div>
-            </div>
-
-            </div>
-        </article>
-        <?php }
-                    mysqli_stmt_close($stmt);
-                }
-                mysqli_close($link);
+                </article>
+            <?php }
+            mysqli_stmt_close($stmt);
+        }
+        mysqli_close($link);
 
 
-                ?>
+        ?>
     </section>
-
 
 
     <!-- COMENTÁRIOS -->
 
     <!-- Button trigger modal -->
-    <div class="modal show margemmodal" id="myModal<?= $id_comentario?>">
+    <div class="modal show margemmodal" id="myModal<?= $id_comentario ?>">
 
         <div class="modal-dialog modal-lg modal-dialog-centered">
 
