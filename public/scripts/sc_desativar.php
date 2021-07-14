14b93f88494fca4bd03ba712e04f7debb90d4679
Joana Lopes Tavares <joanalt@ua.pt>
2021-07-14 20:06:43 +0100

    desativar conta

---
+++ public/scripts/sc_desativar.php
@@ -0,0 +1,32 @@
+<?php
+require_once "../connections/connection.php";
+session_start();
+
+if (isset($_SESSION['id'])) {
+
+    $id_user = $_SESSION['id'];
+
+    $link = new_db_connection();
+
+    $stmt = mysqli_stmt_init($link);
+
+    $query = "DELETE FROM users WHERE users.id_users = ?";
+
+    if (mysqli_stmt_prepare($stmt, $query)) {
+
+        mysqli_stmt_bind_param($stmt, 'i', $id_user);
+
+        //Devemos validar também o resultado do execute!
+        if (mysqli_stmt_execute($stmt)) {
+            //Ação de sucesso
+            header("Location: ../login.php");
+        } else {
+            //Ação de erro
+            echo "Error:" . mysqli_error($link);
+        }
+    } else {
+        echo "Error:" . mysqli_error($link);
+    }
+    mysqli_stmt_close($stmt);
+    mysqli_close($link);
+}
