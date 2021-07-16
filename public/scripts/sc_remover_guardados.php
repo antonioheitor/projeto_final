5feb719da1967369ffb16493d577a4b54882c5b6
Joana Lopes Tavares <joanalt@ua.pt>
2021-07-16 12:46:35 +0100

    teste ao remover dos guardados

--- public/scripts/sc_remover_guardados.php
+++ public/scripts/sc_remover_guardados.php
@@ -1,3 +1,33 @@
 <?php
 require_once "../connections/connection.php";
 session_start();
+
+if (isset($_SESSION['id']) && isset($_GET['post'])) {
+
+    $id_user = $_SESSION['id'];
+    $id_post = $_GET['post'];
+
+    $link = new_db_connection();
+
+    $stmt = mysqli_stmt_init($link);
+
+    $query = "DELETE FROM users_has_posts WHERE users_id_users = ? AND posts_id_posts = ?";
+
+    if (mysqli_stmt_prepare($stmt, $query)) {
+
+        mysqli_stmt_bind_param($stmt, 'ii', $id_user, $id_post);
+
+        //Devemos validar também o resultado do execute!
+        if (mysqli_stmt_execute($stmt)) {
+            //Ação de sucesso
+            header("Location: ../guardados.php");
+        } else {
+            //Ação de erro
+            echo "Error:" . mysqli_error($link);
+        }
+    } else {
+        echo "Error:" . mysqli_error($link);
+    }
+    mysqli_stmt_close($stmt);
+    mysqli_close($link);
+}
