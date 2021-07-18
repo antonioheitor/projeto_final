ee2ced49fc76711a3181df269c24188e2649bbd6
Joana Lopes Tavares <joanalt@ua.pt>
2021-07-18 23:58:57 +0100

    está :p

--- public/scripts/sc_new_post.php
+++ public/scripts/sc_new_post.php
@@ -85,10 +85,10 @@ $data_criacao_post = "2021-07-18";
 
     $stmt = mysqli_stmt_init($link);
 
-    $query = "INSERT INTO posts (titulo_post, conteudo_post, imagem_post, data_criacao_post, users_id_users, grupo_id_grupo ) VALUES (?,?,?,?,?,?)";
+    $query = "INSERT INTO posts (titulo_post, conteudo_post, imagem_post, data_criacao_post, users_id_users, grupo_id_grupo ) VALUES (?,?,?,NOW(),?,?)";
 
     if (mysqli_stmt_prepare($stmt, $query)) {
-        mysqli_stmt_bind_param($stmt, 'sssii', $titulo_post, $conteudo_post, $imagem_post, $data_criacao_post , $users_id_users, $grupo_id_grupo);
+        mysqli_stmt_bind_param($stmt, 'sssii', $titulo_post, $conteudo_post, $imagem_post, $USER_ID, $grupo_id_grupo);
 
         // Devemos validar também o resultado do execute!
         if (mysqli_stmt_execute($stmt)) {
