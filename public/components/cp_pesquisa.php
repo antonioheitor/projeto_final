cbfc30e92bc0c75c149533f21faacb3464d489e7
Mariana Rocha <marianacrocha@ua.pt>
2021-07-19 17:31:06 +0100

    pesquisa check

--- public/components/cp_pesquisa.php
+++ public/components/cp_pesquisa.php
@@ -30,7 +30,7 @@ ON temas.id_temas = grupo.temas_id_temas";
         mysqli_stmt_execute($stmt);
 
 
-        mysqli_stmt_bind_result($stmt, $id_temas, $nome_tema, $areas_id_areas, $id_grupo, $img_grupo); ?>
+        mysqli_stmt_bind_result($stmt, $id_temas, $nome_tema, $areas_id_areas, $id_grupo, $img_grupo, $nome_grupo); ?>
         <main class="container-fluid mt-lg-5">
 
         <div class="galeria row mx-auto my-5 py-5">
@@ -39,7 +39,7 @@ ON temas.id_temas = grupo.temas_id_temas";
             <div class="col-6 col-md-4 col-lg-3 mb-2">
                 <a href="perfil_tribo.php?grupo=<?= $id_grupo ?>">
                     <img src="images/<?=$img_grupo ?>" class="img-fluid m-2 redondo shadow">
-                    <h4 class="text-center ml-2"><?= $nome_tema ?></h4>
+                    <h4 class="text-center ml-2"><?= $nome_grupo ?></h4>
                 </a>
             </div>
 
