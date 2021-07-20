40467e954da17cf326a6c436d6ece686d44906d5
Juliana Gouveia <julyanagouveya@gmail.com>
2021-07-20 17:03:18 +0100

    sc_teste

--- public/scripts/sc_teste.php
+++ public/scripts/sc_teste.php
@@ -9,6 +9,8 @@ $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
 $img_hash = hash('ripemd160', basename($_FILES["teste"]["name"])) . '.'. $imageFileType;
 
+$target_file = $target_dir . $img_hash;
+
 
 // Check if image file is a actual image or fake image
 if (isset($_POST["submit"])) {
@@ -47,7 +49,7 @@ if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
 // if everything is ok, try to upload file
 } else {
-    if (move_uploaded_file($_FILES["teste"]["name"], $target_dir)) {
+    if (move_uploaded_file($_FILES["teste"]["tmp_name"], $target_file)) {
         echo "The file " . htmlspecialchars($img_hash) . " has been uploaded.";
     } else {
         echo "Sorry, there was an error uploading your file.";
