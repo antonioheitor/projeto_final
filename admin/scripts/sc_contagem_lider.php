e93c9b63034abbaf0e91ddfda611942333431b2c
Juliana Gouveia <julyanagouveya@gmail.com>
2021-07-20 19:16:02 +0100

    todas.

---
+++ admin/scripts/sc_contagem_lider.php
@@ -0,0 +1,77 @@
+<?php
+require_once "../connections/connection.php";
+session_start();
+
+if (isset($_SESSION['id']) && isset($_GET['grupo'])) {
+    $user = $_SESSION['id'];
+    $id_grupo = $_GET['grupo'];
+
+
+    $link = new_db_connection();
+
+    $stmt = mysqli_stmt_init($link);
+
+    $query = "SELECT id_quinzena FROM quinzenas ORDER BY id_quinzena ASC LIMIT 1";
+
+    if (mysqli_stmt_prepare($stmt, $query)) {
+        //Devemos validar também o resultado do execute!
+        if (mysqli_stmt_execute($stmt)) {
+            //Ação de sucesso
+            mysqli_stmt_bind_result($stmt, $quinzena);
+            while (mysqli_stmt_fetch($stmt)){
+            }
+
+        } else {
+            //Ação de erro
+            echo "Error:" . mysqli_error($link);
+        }
+    } else {
+        echo "Error:" . mysqli_error($link);
+    }
+    mysqli_stmt_close($stmt);
+
+
+
+    $stmt1 = mysqli_stmt_init($link);
+
+    $query1 ="SELECT votos.users_id_users1, votos.grupo_id_grupo, users.nome_users,
+COUNT(votos.users_id_users1)
+FROM votos
+INNER JOIN users
+ON users.id_users = votos.users_id_users1
+WHERE votos.roles_grupos_id_roles = 1 AND votos.quinzenas_id_quinzena = ? AND votos.grupo_id_grupo = ?
+GROUP BY votos.users_id_users1
+ORDER BY votos.users_id_users DESC LIMIT 1";
+
+    if (mysqli_stmt_prepare($stmt1, $query1)) {
+
+        mysqli_stmt_bind_param($stmt1, 'ii', $quinzena, $id_grupo);
+
+        //Devemos validar também o resultado do execute!
+        if (mysqli_stmt_execute($stmt1)) {
+            //Ação de sucesso
+            mysqli_stmt_bind_result($stmt1, $user_id,  $id_grupo, $user_name, $contagem);
+
+            while (mysqli_stmt_fetch($stmt1)) {
+                $role = 1;
+                header("Location: sc_submeter_votos_lider.php?id=$user_id&grupo=$id_grupo&contagem=$contagem&role=$role");
+            }
+
+            if ($contagem == '') {
+                header("Location: ../../admin/votacoes.php");
+            }
+
+        } else {
+            //Ação de erro
+
+        }
+    } else {
+    }
+    mysqli_stmt_close($stmt1);
+
+}
+
+
+//1 296 000 segundos em 15 dias
+
+
---
+++ admin/scripts/sc_submeter_votos_mestre.php
@@ -0,0 +1,96 @@
+<?php
+
+require_once "../connections/connection.php";
+session_start();
+
+
+if (isset($_SESSION['id']) && (isset($_GET['id']) && (isset($_GET['id'] )!= '')) && (isset($_GET['grupo']) && (isset
+            ($_GET['grupo'] )!= '')) && (isset($_GET['contagem']) && (isset
+            ($_GET['role'] )!= '')) && (isset
+        ($_GET['role'] )!= '')) {
+
+
+    $id_user= $_GET['id'];
+    $id_grupo = $_GET["grupo"];
+    $contagem =  $_GET['contagem'];
+    $role = $_GET['role'];
+
+
+    $link = new_db_connection();
+
+    $stmt = mysqli_stmt_init($link);
+
+    $query = "SELECT users_id_users, grupo_id_grupo, roles_grupos_id_roles FROM users_has_grupo WHERE roles_grupos_id_roles = ? AND grupo_id_grupo = ?";
+
+    if (mysqli_stmt_prepare($stmt, $query)) {
+
+        mysqli_stmt_bind_param($stmt, 'ii', $role, $id_grupo);
+
+        //Devemos validar também o resultado do execute!
+        if (mysqli_stmt_execute($stmt)) {
+            mysqli_stmt_bind_result($stmt, $id_user, $id_grupo, $role);
+
+            echo "CHECK 1";
+
+        } else {
+            //Ação de erro
+            echo "Error:" . mysqli_error($link);
+        }
+    } else {
+        echo "Error:" . mysqli_error($link);
+    }
+    mysqli_stmt_close($stmt);
+
+
+
+    $stmt = mysqli_stmt_init($link);
+
+    $query = "UPDATE users_has_grupo SET roles_grupos_id_roles = 6 WHERE grupo_id_grupo = ? AND users_id_users = ? AND roles_grupos_id_roles = ?";
+
+    if (mysqli_stmt_prepare($stmt, $query)) {
+
+        mysqli_stmt_bind_param($stmt, 'iii',  $id_grupo, $users, $role);
+
+        echo $role;
+        //Devemos validar também o resultado do execute!
+        if (!mysqli_stmt_execute($stmt)) {
+            echo "Error: " . mysqli_stmt_error($stmt);
+        } else {
+            //Ação de erro
+            echo "CHECK 2";
+        }
+    } else {
+        echo "Error:" . mysqli_error($link);
+    }
+    mysqli_stmt_close($stmt);
+
+
+
+    $stmt = mysqli_stmt_init($link);
+
+    $query = "UPDATE users_has_grupo SET roles_grupos_id_roles = ? WHERE users_id_users = ? AND grupo_id_grupo = ?";
+
+    if (mysqli_stmt_prepare($stmt, $query)) {
+
+        echo $role;
+
+        mysqli_stmt_bind_param($stmt, 'iii', $role, $id_user, $id_grupo);
+
+        //Devemos validar também o resultado do execute!
+        if (!mysqli_stmt_execute($stmt)) {
+
+            echo "Error: " . mysqli_stmt_error($stmt);
+
+
+        } else {
+            header("Location: ../../admin/index.php");
+        }
+    } else {
+        echo "Error:" . mysqli_error($link);
+    }
+    mysqli_stmt_close($stmt);
+    mysqli_close($link);
+
+}
+
+
